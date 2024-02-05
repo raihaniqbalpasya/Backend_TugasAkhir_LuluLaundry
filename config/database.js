99da4360790eeb8dd114c4ea042775e57145732b
unknown <19104030@ittelkom-pwt.ac.id>
2024-02-05 09:38:38 +0700

    update env

--- config/database.js
+++ config/database.js
@@ -16,10 +16,10 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME_EL,
+    password: process.env.DB_PASSWORD_EL,
+    database: process.env.DB_NAME_EL,
+    host: process.env.DB_HOST_EL,
     dialect: "postgres",
   },
 };
